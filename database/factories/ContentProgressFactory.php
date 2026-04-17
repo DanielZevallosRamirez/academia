dd0adfe162b465e84c0298d7093fc2dbeae5541b
Daniel Zevallos <daniel.zevallos04@hotmail.com>
2026-04-17 17:24:47 -0500

    Agregue funciones de autenticación y administración de perfiles
    - Se implementó la funcionalidad de registro de usuarios, inicio de sesión y restablecimiento de contraseña.
    - Creé vistas de gestión de perfiles y formularios para actualizar la información del usuario y la contraseña.
    - Se agregaron funciones de verificación de correo electrónico y eliminación de cuenta.
    - Desarrollé componentes Blade reutilizables para campos de entrada, botones y navegación.
    - Rutas establecidas para autenticación y gestión de perfiles.
    - Pruebas incluidas de autenticación, verificación de correo electrónico, restablecimiento de contraseña y actualizaciones de perfil.
    - Tailwind CSS configurado para estilo y diseño responsivo.

---
+++ database/factories/ContentProgressFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ContentProgress;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ContentProgress>
+ */
+class ContentProgressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/PaymentPlanFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PaymentPlan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<PaymentPlan>
+ */
+class PaymentPlanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
